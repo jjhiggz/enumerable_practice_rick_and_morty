57c73ee1b98d88cac60f9b70f27d69bc15344911
jjhiggz <jonathan.higger@gmail.com>
2020-10-29 10:03:17 -0600

    added all files

---
+++ functions.rb
@@ -0,0 +1,27 @@
+require_relative  './data.rb'
+
+  # make a function that creates one large array of all characters' Hashes 
+
+  # Make a function that returns an array of character hashes for a given page number
+
+  # Make a function that returns an array of all of the alive characters
+  
+  # Make a function that returns an array of all the dead characters
+
+  # Make a function that finds all  characters by a given location
+
+  # Make a function that returns the first character with a matching name
+
+  # make a function that counts the amount of male characters on a given page
+
+  # make a function that counts the amount of female characters on a given page
+
+  # make a function that calculates the male to female ratio of characters on a given page
+
+  
+
+  
+  
+
+
+p data
